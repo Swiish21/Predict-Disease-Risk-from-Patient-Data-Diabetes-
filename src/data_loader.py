dbebf4e0f9f1a34f22e5fe376d783ed412f032b0
Malcom Otieno Adrian <malcolmotieno16@gmail.com>
2025-06-18 14:52:58 +0300

    Add initial Jupyter notebook and data loading function(python file) for diabetes dataset

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,1 +1,22 @@
 # Functions to load and return data
+import pandas as pd
+
+def load_diabetes_data(url=None):
+    """
+    Loads the Pima Indians Diabetes Dataset.
+    
+    Parameters:
+        url (str): Optional URL or path to the dataset.
+        
+    Returns:
+        pd.DataFrame: Loaded DataFrame with appropriate column names.
+    """
+    if url is None:
+        url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/pima-indians-diabetes.data.csv"
+    
+    columns = [
+        'Pregnancies', 'Glucose', 'BloodPressure', 'SkinThickness', 'Insulin',
+        'BMI', 'DiabetesPedigreeFunction', 'Age', 'Outcome'
+    ]
+    df = pd.read_csv(url, names=columns)
+    return df
